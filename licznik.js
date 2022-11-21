1e35f80c233d4432b9bad2f3231e7ad1226bdb20
hugminsub <hugon.grabek@gmail.com>
2022-11-21 21:00:44 +0100

    wip

--- licznik.js
+++ licznik.js
@@ -3,12 +3,21 @@ let suma = 1;
 console.log(suma)
 suma += 1;
 
-document.getElementById("wynik").value=suma
+function showScore(){
+	document.getElementById("wynik").value=suma
+
+}
+
+	showScore()
 document.getElementById("plus").addEventListener("click", function(){
 	suma +=1;
-	document.getElementById("wynik").value=suma
+	showScore()
 })
 document.getElementById("minus").addEventListener("click", function(){
 	suma -=1;
-	document.getElementById("wynik").value=suma
+	showScore()
+})
+document.getElementById("resetuj").addEventListener("click", function(){
+	suma=0
+	showScore()
 })
